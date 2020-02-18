b48ea63e9d32470e9c2e0d0c8e3cdbb782747ff7
Derrick Aung <derek.aungkhant@protonmail.com>
2020-02-18 22:07:39 +0630

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import { BrowserRouter,Route, Switch } from 'react-router-dom'
 import './App.css';
+import ProMatches from './components/ProMatches';
+import ProMatchDetail from './components/ProMatchDetail'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/rootReducer.js'
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunk),
+))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store = {store}>
+      <div className="App">
+        <header className="App-header">
+          <BrowserRouter>
+            <Navbar />
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/pro-matches">
+                <ProMatches />
+              </Route>
+              <Route path="/pro-matches/:match_id" component={ProMatchDetail}/>
+            </Switch>
+          </BrowserRouter>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
